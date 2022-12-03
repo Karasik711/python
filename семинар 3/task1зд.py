00d33042786c8c63f37e7d9419020691c49fd95a
Ksenia <karasevaksenia3644@gmail.com>
2022-12-03 19:19:59 +0300

    семинар 3, дз 1

---
+++ семинар 3/task1зд.py
@@ -0,0 +1,16 @@
+# Задайте список из нескольких чисел. Напишите программу, 
+# которая найдёт сумму элементов списка, стоящих 
+# на нечётной идексах.
+# Пример:
+# [2, 3, 5, 9, 3] -> на нечётных идексах элементы 3 и 9, 
+# ответ: 12
+
+import random
+random_list = [random.randint(1, 10) for i in range(1, 11)]
+print(random_list)
+result_sum = 0
+result_list = []
+for i in range(1, len(random_list), 2):
+    result_sum += random_list[i]
+    result_list.append(random_list[i])
+print(f'На нечётных позициях элементы {result_list},\nих сумма равна {result_sum}.')
